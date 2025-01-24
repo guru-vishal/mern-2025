1d8bc4f1d82cf69f6eef532db1ac6d10ef74abf9
guru-vishal <vishal3012006@gmail.com>
2025-01-24 09:15:33 +0530

    Day 6 AN

---
+++ MERN Day 6/AN/src/App.jsx
@@ -0,0 +1,46 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './components/Home';
+import Comone from './components/FunctionalComponents/Comone';
+import Comtwo from './components/FunctionalComponents/Comtwo';
+import Comthree from './components/FunctionalComponents/Comthree';
+import Comfour from './components/FunctionalComponents/Comfour';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+    <div>
+      <Home/>
+      <Comone/>
+      <Comtwo/>
+      <Comthree/>
+      <Comfour/>
+    </div>
+    </div>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
