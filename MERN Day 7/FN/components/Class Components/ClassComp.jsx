dc6606652ddda624c343e518bf959829f64762f6
guru-vishal <vishal3012006@gmail.com>
2025-01-24 09:35:21 +0530

    Day 7 FN

---
+++ MERN Day 7/FN/components/Class Components/ClassComp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+class ClassComp extends React.Component{
+    render(){
+        return(
+            <div> This is a class Component Example </div>
+        )
+    }
+}
+export default ClassComp;
---
+++ MERN Day 7/FN/src/App.jsx
@@ -0,0 +1,17 @@
+import About from './assets/components/classComponents/Functional Components/About'
+import ClassCompEG from './assets/components/classComponents/ClassCompEG'
+import Home from './assets/components/classComponents/Functional Components/home'
+
+function App() {
+    return (
+    <>
+      <div>
+         <Home/>
+        <About/>
+        <ClassCompEG/>
+      </div>
+    </>
+  );
+}
+
+export default App;
